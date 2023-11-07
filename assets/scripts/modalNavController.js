1748661eaa985d7f16c72aa8bdd159af600b560e
Jason Sherrick <jasonsherrick@gmail.com>
2023-11-07 15:47:19 -0600

    added script to control modal nav

---
+++ assets/scripts/modalNavController.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', e => {
+    const openButton = document.getElementById('openButton');
+    const closeButton = document.getElementById('closeButton');
+    const modal = document.getElementById('navModal');
+
+    openButton.addEventListener('click', e => {
+        openButton.classList.toggle('__toggle');
+        modal.classList.toggle('__toggle');
+        closeButton.classList.toggle('__toggle');
+    })
+})
